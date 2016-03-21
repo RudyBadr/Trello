ebec30831f69649d9b6682177d198e3a28fc63be
Rudy Zidan <rudy.zidan@badrit.com>
2016-03-21 13:29:53 +0200

    Verify user sign in system correctly redirect to dashboard screen.

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
 
   devise_for :users
   get 'dashboard/index'
+  get 'dashboard/profile'
 
 
   # The priority is based upon order of creation: first created -> highest priority.
