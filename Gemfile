8817fb34835cfd3767b015a9b02495bb39dce0a6
Rudy Zidan <rudy.zidan@badrit.com>
2016-03-23 11:30:23 +0200

    Create index page in dashboard to list all projects with edit, stories link for full control.

--- Gemfile
+++ Gemfile
@@ -58,3 +58,5 @@ gem "paperclip", "~> 4.3"
 
 gem 'remotipart', '~> 1.2'
 
+gem 'will_paginate', '~> 3.0.6'
+
--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,6 +1,6 @@
 class DashboardController < ApplicationController
   def index
-  	logger.debug current_user.projects_joined.inspect
+  	
   end
   def profile
   end
